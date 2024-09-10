dc0b1302cd07d3febec6df6e761f0593c05257cc
alizayJ <116749097+alizayJ@users.noreply.github.com>
2024-09-11 00:28:56 +0500

    Add files via upload

---
+++ learn1.py
@@ -0,0 +1,30 @@
+print("My name is Aliza" , "My age is 19") #will print this on same line
+print(20 % 10)
+print(3 % 5)
+
+name = "Aliza"
+lastname = "kashif"
+print("My name is : ", name)
+print(name + lastname) #concatenation
+price = 25.99
+print(type(price))
+a = None 
+print(type(a))
+
+# Expression Execution
+A, B = 2, 3
+txt = "@"
+print(A*txt*B)
+
+C, D = "2", 3
+print((C + txt)* D)
+
+# multi line comment
+""" bhdhcbhbdcbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbnx nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbm xmsnmsncjdsnckjsnckjnckjndjkcnkdjnck"""
+''' bhdhcbhbdcbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbnx nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbm xmsnmsncjdsnckjsnckjnckjndjkcnkdjnck'''
+
+yourName = input("name: ") # for strings and sentences input
+age = int(input("age: "))
+floatvalue = float(input("Price: "))
+print("My name is", yourName, "my age is",age) # gives space after each sentence ending automatically
+print(2**3)
